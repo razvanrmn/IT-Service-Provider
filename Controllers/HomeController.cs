59560591d41512f78a98490b404c324bcbbe6409
Roman Razvan <98484095+razvanrmn@users.noreply.github.com>
2023-01-18 21:38:36 +0200

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
-namespace IT_Service_Provider.Controllers
+namespace MultiLanguage.Controllers
 {
     public class HomeController : Controller
     {
+        DateTimeOffset dateTimeNow = DateTimeOffset.Now;
         public ActionResult Index()
         {
+            Singleton.Instance.writeMessage("Language button clicked at: " + dateTimeNow.LocalDateTime);
             return View();
         }
 
@@ -25,6 +29,19 @@ namespace IT_Service_Provider.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Change(string LanguageAbbreviation)
+        {
+            if (LanguageAbbreviation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbbreviation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageAbbreviation);
+            }
+            HttpCookie cookie = new HttpCookie("Home");
+            cookie.Value = LanguageAbbreviation;
+            Response.Cookies.Add(cookie);
+            Singleton.Instance.writeMessage("Language changed succesfuly at: " + dateTimeNow.LocalDateTime);
+            return View("Index");
         }
     }
 }
---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,47 @@
+﻿using MultiLanguage;
+using MultiLanguage.Models;
+using ProiectFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProiectFinal.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private MainDbContext context = new MainDbContext();
+        DateTimeOffset dateTimeNow = DateTimeOffset.Now;
+        public ActionResult Index()
+        {
+            Singleton.Instance.writeMessage("Review index button clicked created at: " + dateTimeNow.LocalDateTime);
+            return View(context.Reviews.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create() 
+        { 
+            ReviewModel review= new ReviewModel();
+            Singleton.Instance.writeMessage("Create review succesfuly at: " + dateTimeNow.LocalDateTime);
+            return View(review);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ReviewModel review)
+        {
+            context.Reviews.Add(review);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Sort() 
+        {
+            var res = from r in context.Reviews
+                      orderby r.NumberOfStars
+                      select r;
+            return View(res);
+        }
+    }
+}
