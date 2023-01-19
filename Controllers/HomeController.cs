3249a3577bff58968eecb130ccdd259c49ffa672
Roman Razvan <98484095+razvanrmn@users.noreply.github.com>
2023-01-19 11:45:02 +0200

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
-namespace IT_Service_Provider.Controllers
+namespace MultiLanguage.Controllers
 {
     public class HomeController : Controller
     {
+        DateTimeOffset dateTimeNow = DateTimeOffset.Now;
         public ActionResult Index()
         {
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
+            Singleton.Instance.writeMessage("Language button clicked at: " + dateTimeNow.LocalDateTime);
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
             return View();
+        }
+        public ActionResult Change(string LanguageAbbreviation)
+        {
+            if (LanguageAbbreviation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbbreviation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageAbbreviation);
+            }
+            HttpCookie cookie = new HttpCookie("Home");
+            cookie.Value = LanguageAbbreviation;
+            Response.Cookies.Add(cookie);
+            Singleton.Instance.writeMessage("Language changed succesfuly at: " + dateTimeNow.LocalDateTime);
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
+            return View("Index");
         }
     }
 }
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using MultiLanguage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanguage.Controllers
+{
+    public class ProductController : Controller
+    {
+        private MainDbContext context = new MainDbContext();
+        DateTimeOffset dateTimeNow = DateTimeOffset.Now;
+
+        public ActionResult Index()
+        {
+            Singleton.Instance.writeMessage("Product index button clicked at: " + dateTimeNow.LocalDateTime);
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
+            return View(context.Products.ToList());
+        }
+        [HttpGet]
+        public ActionResult Buy()
+        {
+            ProductModel model = new ProductModel();
+            Singleton.Instance.writeMessage("Buy button clicked at: " + dateTimeNow.LocalDateTime);
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Buy(ShoppingListModel model)
+        {
+            Singleton.Instance.writeMessage("Buy button clicked at: " + dateTimeNow.LocalDateTime);
+            context.ShoppingList.Add(model);
+            context.SaveChanges();
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ViewDetails(int id)
+        {
+            ProductModel details = context.Products.Find(id);
+            if (details == null)
+            {
+                return HttpNotFound();
+            }
+            Singleton.Instance.writeMessage("View product details button clicked at: " + dateTimeNow.LocalDateTime);
+            Thread t = new Thread(new ThreadStart(Singleton.WriteToConsole));
+            t.Start();
+            return View(details);
+        }
+    }
+}
